5aa851f8d62c848040f701f4adf35050c6b38e55
Ger Hobbelt <ger@hobbelt.com>
2022-01-06 20:31:03 +0100

    adding support for compiling as DLL on MSWindows: step 1: introduce the dlimpeexp.h header file, which is ripped from wxWidgets itself, cleaned up and WX* --> WXCHARTS_* to ensure we can create and manage wxCharts as an independent DLL.

---
+++ include/wx/charts/wxchart_dlimpexp.h
@@ -0,0 +1,100 @@
+/*
+ * Name:        wx/dlimpexp.h
+ * Purpose:     Macros for declaring DLL-imported/exported functions
+ * Author:      Vadim Zeitlin
+ * Modified by:
+ * Created:     16.10.2003 (extracted from wx/defs.h)
+ * Copyright:   (c) 2003 Vadim Zeitlin <vadim@wxwidgets.org>
+ * Licence:     wxWindows licence
+ */
+
+/*
+    This is a C file, not C++ one, do not use C++ comments here!
+ */
+
+#ifndef _WX_CHARTS_DLIMPEXP_H_
+#define _WX_CHARTS_DLIMPEXP_H_
+
+#include <wx/platform.h>
+
+#if defined(WXCHARTS_STATICLIB)
+#    define WXCHARTS_EXPORT
+#    define WXCHARTS_IMPORT
+#elif defined(HAVE_VISIBILITY)
+#    define WXCHARTS_EXPORT __attribute__ ((visibility("default")))
+#    define WXCHARTS_IMPORT __attribute__ ((visibility("default")))
+#elif defined(__WINDOWS__)
+    /*
+       __declspec works in as VC++.
+     */
+#    if defined(__VISUALC__)
+#        define WXCHARTS_EXPORT __declspec(dllexport)
+#        define WXCHARTS_IMPORT __declspec(dllimport)
+    /*
+        While gcc also supports __declspec(dllexport), it created unusably huge
+        DLL files in gcc 4.[56] (while taking horribly long amounts of time),
+        see http://gcc.gnu.org/bugzilla/show_bug.cgi?id=43601. Because of this
+        we rely on binutils auto export/import support which seems to work
+        quite well for 4.5+. However the problem was fixed in 4.7 and later and
+        not exporting everything creates smaller DLLs (~8% size difference), so
+        do use the explicit attributes again for the newer versions.
+     */
+#    elif defined(__GNUC__) && \
+        (!wxCHECK_GCC_VERSION(4, 5) || wxCHECK_GCC_VERSION(4, 7))
+        /*
+            __declspec could be used here too but let's use the native
+            __attribute__ instead for clarity.
+        */
+#       define WXCHARTS_EXPORT __attribute__((dllexport))
+#       define WXCHARTS_IMPORT __attribute__((dllimport))
+#    endif
+#elif defined(__CYGWIN__)
+#    define WXCHARTS_EXPORT __declspec(dllexport)
+#    define WXCHARTS_IMPORT __declspec(dllimport)
+#endif
+
+/* for other platforms/compilers we don't anything */
+#ifndef WXCHARTS_EXPORT
+#    define WXCHARTS_EXPORT
+#    define WXCHARTS_IMPORT
+#endif
+
+/*
+   WXCHARTS_DLLIMPEXP maps to export declaration when building the DLL, to import
+   declaration if using it or to nothing at all if we don't use wxCharts as DLL
+ */
+#ifdef WXCHARTS_MAKINGDLL
+#    define WXCHARTS_DLLIMPEXP WXCHARTS_EXPORT
+#    define WXCHARTS_DLLIMPEXP_DATA(type) WXCHARTS_EXPORT type
+#    if defined(HAVE_VISIBILITY)
+#        define WXCHARTS_DLLIMPEXP_INLINE WXCHARTS_EXPORT
+#    else
+#        define WXCHARTS_DLLIMPEXP_INLINE
+#    endif
+#elif defined(WXCHARTS_USINGDLL)
+#    define WXCHARTS_DLLIMPEXP WXCHARTS_IMPORT
+#    define WXCHARTS_DLLIMPEXP_DATA(type) WXCHARTS_IMPORT type
+#    if defined(HAVE_VISIBILITY)
+#        define WXCHARTS_DLLIMPEXP_INLINE WXCHARTS_IMPORT
+#    else
+#        define WXCHARTS_DLLIMPEXP_INLINE
+#    endif
+#else /* not making nor using DLL */
+#    define WXCHARTS_DLLIMPEXP
+#    define WXCHARTS_DLLIMPEXP_DATA(type) type
+#    define WXCHARTS_DLLIMPEXP_INLINE
+#endif
+
+/*
+   GCC warns about using __attribute__ (and also __declspec in mingw32 case) on
+   forward declarations while MSVC complains about forward declarations without
+   __declspec for the classes later declared with it, so we need a separate set
+   of macros for forward declarations to hide this difference:
+ */
+#if defined(HAVE_VISIBILITY) || (defined(__WINDOWS__) && defined(__GNUC__))
+    #define WXCHARTS_DLLIMPEXP_FWD
+#else
+    #define WXCHARTS_DLLIMPEXP_FWD      WXCHARTS_DLLIMPEXP
+#endif
+
+#endif /* _WX_DLIMPEXP_H_ */
